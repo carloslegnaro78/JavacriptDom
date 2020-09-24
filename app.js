d733a265ec3197a0401465e37374b54e1d04c1e6
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-09-24 16:59:14 -0300

    GETELEMENTSBYTAGNAME JAVASCRIPT

--- app.js
+++ app.js
@@ -7,9 +7,19 @@ setTimeout(function () {
     //document.getElementById('test').innerText = 'Sou um novo texto'
     //console.log(document.getElementsByName('div-name')[0].innerText ='Alterado o primeiro')
 
-    var nameTag = document.getElementsByName('div-name')
-    nameTag[0].innerText = 'Alterei...'
-    nameTag[1].innerText = 'Alterei o segundo...'
+    //var nameTag = document.getElementsByName('div-name')
+    //nameTag[0].innerText = 'Alterei...'
+    //nameTag[1].innerText = 'Alterei o segundo...'
     //document.getElementsByName('div-name')[1].innerText ='Alterando o segundo'
+
+    //console.log(document.getElementsByTagName('div')[0].innerHTML = '<h4> Mudando...</h4>')
+
+    var tagsName = document.getElementsByTagName('div')
+    tagsName[0].innerHTML = '<h4> Mudando 1...</h4>'
+    tagsName[1].innerHTML = '<h4> Mudando 2...</h4>'
+    
+    if(tagsName[2]){
+        tagsName[2].innerHTML = '<h4> Mudando 3...</h4>'
+    }
 
 }, 3000)
