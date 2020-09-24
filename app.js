34e19ca06282d3eb2936e668ea53306ced16d5fd
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-09-24 15:08:23 -0300

    Inicio

---
+++ app.js
@@ -0,0 +1,9 @@
+//console.log(document.title = 'Javacript DOM')
+//document.write('Sou o uso do document.write')
+//document.getElementById('test').innerHTML = 'Sou um novo texto'
+
+setTimeout(function(){
+    //document.getElementById('test').innerHTML = 'Sou um novo texto'
+    document.getElementById('test').innerText = 'Sou um novo texto'
+    //console.log(document.getElementsByName('div-name'))
+},3000)
